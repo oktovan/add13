de817165d91002324c0abcf193a40d8eb0fb8ae0
oktovan <oktovan@users.noreply.github.com>
2020-10-11 18:43:12 +0700

    Create sudoadd13.sh

---
+++ sudoadd13.sh
@@ -0,0 +1,25 @@
+sudo git clone https://github.com/oktovan/prune12
+
+sudo git clone -b 12.0 https://github.com/oca/account-analytic
+sudo git clone -b 12.0 https://github.com/OCA/account-closing
+sudo git clone -b 12.0 https://github.com/oca/account-invoicing
+sudo git clone -b 12.0 https://github.com/oca/account-financial-reporting
+sudo git clone -b 12.0 https://github.com/oca/account-financial-tools 
+sudo git clone -b 12.0 https://github.com/oca/contract
+sudo git clone -b 12.0 https://github.com/oca/intrastat
+sudo git clone -b 12.0 https://github.com/oca/mis-builder
+sudo git clone -b 12.0 https://github.com/oca/manufacture
+sudo git clone -b 12.0 https://github.com/oca/operating-unit
+sudo git clone -b 12.0 https://github.com/oca/partner-contact
+sudo git clone -b 12.0 https://github.com/OCA/purchase-workflow
+sudo git clone -b 12.0 https://github.com/oca/product-attribute
+sudo git clone -b 12.0 https://github.com/oca/reporting-engine
+sudo git clone -b 12.0 https://github.com/OCA/sale-workflow
+sudo git clone -b 12.0 https://github.com/oca/server-tools
+sudo git clone -b 12.0 https://github.com/oca/server-ux.git
+sudo git clone -b 12.0 https://github.com/oca/stock-logistics-transport
+sudo git clone -b 12.0 https://github.com/oca/stock-logistics-warehouse
+sudo git clone -b 12.0 https://github.com/oca/stock-logistics-workflow
+sudo git clone -b 12.0 https://github.com/oca/web
+sudo git clone -b 12.0 https://github.com/OCA/product-variant
+sudo git clone -b 12.0 https://github.com/OCA/website
