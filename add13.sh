0f45fd4c1cb40343309d04d909da7cd642185dc6
oktovan <oktovan@users.noreply.github.com>
2020-10-11 18:42:15 +0700

    add13.sh

---
+++ add13.sh
@@ -0,0 +1,25 @@
+git clone https://github.com/oktovan/prune12
+
+git clone -b 12.0 https://github.com/oca/account-analytic
+git clone -b 12.0 https://github.com/OCA/account-closing
+git clone -b 12.0 https://github.com/oca/account-invoicing
+git clone -b 12.0 https://github.com/oca/account-financial-reporting
+git clone -b 12.0 https://github.com/oca/account-financial-tools 
+git clone -b 12.0 https://github.com/oca/contract
+git clone -b 12.0 https://github.com/oca/intrastat
+git clone -b 12.0 https://github.com/oca/mis-builder
+git clone -b 12.0 https://github.com/oca/manufacture
+git clone -b 12.0 https://github.com/oca/operating-unit
+git clone -b 12.0 https://github.com/oca/partner-contact
+git clone -b 12.0 https://github.com/OCA/purchase-workflow
+git clone -b 12.0 https://github.com/oca/product-attribute
+git clone -b 12.0 https://github.com/oca/reporting-engine
+git clone -b 12.0 https://github.com/OCA/sale-workflow
+git clone -b 12.0 https://github.com/oca/server-tools
+git clone -b 12.0 https://github.com/oca/server-ux.git
+git clone -b 12.0 https://github.com/oca/stock-logistics-transport
+git clone -b 12.0 https://github.com/oca/stock-logistics-warehouse
+git clone -b 12.0 https://github.com/oca/stock-logistics-workflow
+git clone -b 12.0 https://github.com/oca/web
+git clone -b 12.0 https://github.com/OCA/product-variant
+git clone -b 12.0 https://github.com/OCA/website
